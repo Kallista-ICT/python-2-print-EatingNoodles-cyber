dd17fa802ea7bde20c21dc4290922a6659ab2c51
EatingNoodles-cyber <gadisannisarayhana@gmail.com>
2025-01-21 07:28:29 +0000

    submit

---
+++ python_practice/next_year.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+
+#hello, these codes needs variable and uses function a lot, the codes has other alternatives to do it
+name = input("Enter your name: ")
+print(f"Hello {name}")
+age = int(input("Enter your age: "))
+print(f"Hello, {name}! You are {age} years old.")
+print(f"Next year you will be {age + 1} years old.")
