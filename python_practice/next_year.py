29eb8d8927d68e479633282aa25eb8e4ac1ae850
EatingNoodles-cyber <gadisannisarayhana@gmail.com>
2025-02-11 07:06:19 +0000

    submit

--- python_practice/next_year.py
+++ python_practice/next_year.py
@@ -1,8 +1,11 @@
 print("Hello, World!")
 
-#hello, these codes needs variable and uses function a lot, the codes has other alternatives to do it
+#these codes needs variable and uses function a lot, the codes has other alternatives to do it
+#to start is forthe user to input the name
 name = input("Enter your name: ")
+#this code will make the users name automatically in to the sentence that will be told
 print(f"Hello {name}")
 age = int(input("Enter your age: "))
 print(f"Hello, {name}! You are {age} years old.")
+#This is the code to add/calculate the age using the f-string
 print(f"Next year you will be {age + 1} years old.")
